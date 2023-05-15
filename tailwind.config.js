bd461ecccadf6d65f18b36fd2f0ff29d8a403ea0
wettle-emma <emma.wettle@edu.univ-fcomte.fr>
2023-05-15 18:32:31 +0200

    ajout page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.html, js"],
+  content: ["./pages/**/*.{html,js}",
+  "./components/**/*.{html,js}"],
   theme: {
     extend: {},
   },
