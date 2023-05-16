278cb23d25aa434602d33325aceb5be1381a510a
wettle-emma <emma.wettle@edu.univ-fcomte.fr>
2023-05-16 11:00:02 +0200

    commit

--- Panierlocal/src/App.vue
+++ Panierlocal/src/App.vue
@@ -1,40 +1,16 @@
 <script setup lang="ts">
-	import { RouterLink, RouterView } from 'vue-router'
+	import { RouterView } from 'vue-router'
+  import Footer from './components/footer.vue';
+  import Header from './components/header.vue';
 </script>
 
 <template>
-  <header>
-    <nav>
-      <RouterLink to="/">Page d'accueil</RouterLink>
-			<RouterLink to="/ajoutAstuces">Ajout Astuces</RouterLink>
-      <RouterLink to="/ajoutContenu">Ajout Contenu</RouterLink>
-      <RouterLink to="/ajoutEvenement">Ajout Evènement</RouterLink>
-      <RouterLink to="/ajoutProduit">Ajout Produit</RouterLink>
-      <RouterLink to="/ajoutRecette">Ajout Recette</RouterLink>
-      <RouterLink to="/astuce">Astuces</RouterLink>
-      <RouterLink to="/cgv">CGV</RouterLink>
-      <RouterLink to="/cookies">Politique de Cookies</RouterLink>
-      <RouterLink to="/equipe">Qui sommes nous ?</RouterLink>
-      <RouterLink to="/evenements">Evènements</RouterLink>
-      <RouterLink to="/favoris">Favoris</RouterLink>
-      <RouterLink to="/forum">Forum</RouterLink>
-      <RouterLink to="/inscription">Inscription</RouterLink>
-      <RouterLink to="/mentionsLegales">Mentions Légales</RouterLink>
-      <RouterLink to="/messagerie">Messagerie</RouterLink>
-      <RouterLink to="/messages">Messages</RouterLink>
-      <RouterLink to="/pageAstuces">Astuces</RouterLink>
-      <RouterLink to="/pageEvenement">Présentation évènements</RouterLink>
-      <RouterLink to="/pageProduits">Produits</RouterLink>
-      <RouterLink to="/pageRecettes">Recettes</RouterLink>
-      <RouterLink to="/panier">Panier</RouterLink>
-      <RouterLink to="/parametres">Paramètres</RouterLink>
-      <RouterLink to="/payer">Payer</RouterLink>
-      <RouterLink to="/produit">Produit</RouterLink>
-      <RouterLink to="/profil1">Profil 1</RouterLink>
-      <RouterLink to="/profil2">Profil 2</RouterLink>
-      <RouterLink to="/recette">Recette</RouterLink>
-    </nav>
-  </header>
+  <Header />
 
+  <main>
   <RouterView />
+  </main>
+
+  <Footer />
+
 </template>
---
+++ Panierlocal/src/pages/rechercher.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>Rechercher</h1>
+</template>
