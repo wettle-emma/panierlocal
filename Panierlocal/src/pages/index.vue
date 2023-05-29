e6402b0d35bd1073d62960bcf4e559d8c459f0ad
wettle-emma <emma.wettle@edu.univ-fcomte.fr>
2023-05-29 12:58:34 +0200

    pocketbase + photos

--- Panierlocal/src/pages/index.vue
+++ Panierlocal/src/pages/index.vue
@@ -1,3 +1,15 @@
+<script setup lang="ts">
+import logo from '@/components/icons/logo/logo.vue';
+import ChevronDown from '@/components/icons/30px/ChevronDown.vue';
+</script>
+
 <template>
-    <h1>Page d'accueil</h1>
+    <logo />
+    <p class="text-center">Panier local permet à chacun d’agir à son échelle contre le gaspillage. Tout en agissant pour la planète. Panier local nous rapproche pour une cause qui en vaut la peine.</p>
+
+    <ChevronDown />
+    
+    <div>
+        
+    </div>
 </template>
